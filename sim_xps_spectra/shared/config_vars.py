a5995ce3274b021944da793a500c552d47db7c37
Richard M Fogarty <richard.m.fogarty@gmail.com>
2020-02-08 12:34:49 +0000

    shared: Slight change to config_vars file

--- sim_xps_spectra/shared/config_vars.py
+++ sim_xps_spectra/shared/config_vars.py
@@ -1,7 +1,7 @@
 
 import os
 
-BASE_DATA_DIR_PATH = "/run/media/windowsMain/Users/polic/Documents/work2/Random_Crap/sim_xps_spectra/data"
+BASE_DATA_DIR_PATH = "/run/media/windowsMain/Users/polic/Documents/work2/Random_Crap/sim_xps_spectra"
 
-YEH_LINDAU_DB_PATH = os.path.join(BASE_DATA_DIR_PATH,"cross_sections","yeh_lindau")
+YEH_LINDAU_DB_PATH = os.path.join(BASE_DATA_DIR_PATH,"data","cross_sections","yeh_lindau")
 
