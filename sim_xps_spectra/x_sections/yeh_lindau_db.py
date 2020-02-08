bb8a9d788e265983c03797ec6f6bdae57a5926af
Richard M Fogarty <richard.m.fogarty@gmail.com>
2020-02-08 09:32:18 +0000

    x_sections: Added ability to use lower angular momentum cross sections to polarisation basis functions
    For example, to use C2P cross section for C2D orbitals

--- sim_xps_spectra/x_sections/yeh_lindau_db.py
+++ sim_xps_spectra/x_sections/yeh_lindau_db.py
@@ -7,21 +7,31 @@ from ..shared import config_vars as cfgVars
 
 class YehLindauXSectionDatabase(baseObjs.CrossSectionDatabaseBase):
 
-	def __init__(self):
+	def __init__(self, substituteLabelDict=None):
 		""" Database class with Yeh and Lindau cross sections taken "from https://vuo.elettra.eu/services/elements/WebElements.html" who in turn list these references:
 		
 		J.J. Yeh, Atomic Calculation of Photoionization Cross-Sections and Asymmetry Parameters, Gordon and Breach Science Publishers, Langhorne, PE (USA), 1993
 		J.J. Yeh and I.Lindau, Atomic Data and Nuclear Data Tables, 32, 1-155 (1985)
 		"""
 		self._basePath = cfgVars.YEH_LINDAU_DB_PATH
+		if substituteLabelDict is None:
+			self._subLabelDict = createSubstituteLabelDict()
+		else:
+			self._subLabelDict = substituteLabelDict
 
 	def getHvAgainstAOCrossSections(self, label):
+		label = self._getConvertedLabel(label)
 		parsedFile = self._parseFileForLabel(label)
 		return parsedFile["xSections".lower()]
 
 	def getHvAgainstAOAsymFactors(self, label):
+		label = self._getConvertedLabel(label)
 		parsedFile = self._parseFileForLabel(label)
 		return parsedFile["asymFactors".lower()]
+
+	def _getConvertedLabel(self,label):
+		return self._subLabelDict.get(label, label) #returns input label if its not one that needs substituting
+
 
 	def _parseFileForLabel(self, label):
 		inpPath = self._getPathForLabel(label)
@@ -30,6 +40,15 @@ class YehLindauXSectionDatabase(baseObjs.CrossSectionDatabaseBase):
 	def _getPathForLabel(self, label):
 		inpPath = os.path.join(self._basePath, label.upper())
 		return inpPath + ".txt"
+
+
+#This is to make up for the fact that we use polarization functions in our basis set that we cant get an x-section for
+def createSubstituteLabelDict():
+	outDict = {"CL3D":"CL3P",
+	           "C2D":"C2P",
+	           "N2D":"N2P",
+	           "S3D":"S3P"}
+	return outDict	
 
 
 #TODO: Figure out why we have 3 values for x-sections and asym factors. For C2p the asym factor values differ a bit
